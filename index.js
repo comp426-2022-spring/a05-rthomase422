6762cafa44c8d1829ddff8148e34f36ffece6218
rthomase422 <rthomase@live.unc.edu>
2022-04-22 18:45:50 -0400

    added dependencies

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Place your server entry point code here
+// Serve static HTML files
+app.use(express.static('./public'));
+// Make Express use its own built-in body parser to handle JSON
+app.use(express.json());
